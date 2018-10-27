0cb3f3eb4e3654d9484fa31d3fcf9344b024bbed
TSDCH Robotics (LabPC-2827) <LabPC-2827@robotics.tsdch>
2018-10-27 12:00:56 -0400

    PK: remove redundant imports

--- BasicAuto.java
+++ BasicAuto.java
@@ -39,18 +39,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-
-
-import android.app.Activity;
-import android.graphics.Color;
-import android.view.View;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-
-
 @Autonomous(name="Basic", group="BBot")
 public class BasicAuto extends LinearOpMode {
     BBot robot = new BBot();   // Use robot's hardware
