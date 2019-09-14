1c4324b6f5dd86bf24ba7bba23cef0089191c7c1
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2019-09-14 11:56:27 -0400

    remove encoder test bench, move final STB to master

--- Testbench.java
+++ Testbench.java
@@ -1,36 +1,3 @@
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -38,8 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 @TeleOp(name="Servo Test Bench", group="BBot")
 public class ServoTestBench extends OpMode {
@@ -68,7 +33,7 @@ public class ServoTestBench extends OpMode {
             for(i = 0; i < 4; i++) motors[i] = hardwareMap.dcMotor.get(String.format("Motor%d", i));
         }
         catch(Exception ex) {
-            telemetry.addLine("Incorrect config selected! Must use STB configuration.");
+            telemetry.addLine("Incorrect config selected! Must use Testbench configuration.");
         }
     }
 
@@ -130,7 +95,7 @@ public class ServoTestBench extends OpMode {
         telemetry.addLine("## DPad U/D: select standard servo [0-2]; A/B: move -/+");
         telemetry.addLine("## L bumper/trigger: select CR servo [3-5]; L stick: power");
         telemetry.addLine("## R bumper/trigger: select motor [0-3]; R stick: power");
-        telemetry.addData("Selected servo, CR servo, motor", "%d, %d, %d", selectedServo, selectedCRServo + 3, selectedMotor );
+        telemetry.addData("Selected servo, CR servo, motor", "%d, %d, %d", selectedServo, selectedCRServo + 3, selectedMotor);
         telemetry.addData("Servo positions", "%.2f %.2f %.2f", servoPosition[0], servoPosition[1], servoPosition[2]);
         telemetry.addData("CR servo speed", "%.2f", CRServoPower);
         telemetry.addData("Motor speed", "%.2f", MotorPower);
