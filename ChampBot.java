1bf3120096348c2a6af7d5cbbd89bea73ab34985
Trinity School Robotics <trinityrobotics@tsdch.org>
2020-01-27 17:03:27 -0500

    RE: added Motion and test auto (WIP)

--- ChampBot.java
+++ ChampBot.java
@@ -43,10 +43,7 @@ public class ChampBot {
 
     public ChampBot() { }
 
-    // gyro init
-    Gyro gyro;
-    //gyro = new Gyro(hwMap, "imu"); // special initialization for gyro
-    //gyro.start();
+
 
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
@@ -153,65 +150,6 @@ public class ChampBot {
         double countsPerInch = countsPerWheelRotation / wheelCircumference;
 
         return (int)(countsPerInch * inches);
-    }
-
-    public void turnToHeading(double target_heading, double speedModifier) {
-        boolean goRight;
-        double currentHeading;
-        double degreesToTurn;
-        double wheelPower;
-        double prevHeading = 0;
-        ElapsedTime timeoutTimer = new ElapsedTime();
-
-        currentHeading = gyro.getHeading();
-        degreesToTurn = Math.abs(target_heading - currentHeading);
-
-        goRight = target_heading > currentHeading;
-
-        if (degreesToTurn > 180) {
-            goRight = !goRight;
-            degreesToTurn = 360 - degreesToTurn;
-        }
-
-        timeoutTimer.reset();
-        prevHeading = currentHeading;
-        while (degreesToTurn > .5 /*&& opModeIsActive()*/ && timeoutTimer.seconds() < 2) {
-
-            if (speedModifier < 0) {
-                wheelPower = (Math.pow((degreesToTurn + 25) / -speedModifier, 3) + 15) / 100;
-            } else {
-                if (speedModifier != 0) {
-                    wheelPower = (Math.pow((degreesToTurn) / speedModifier, 4) + 35) / 100;
-                } else {
-                    wheelPower = (Math.pow((degreesToTurn) / 30, 4) + 15) / 100;
-                }
-            }
-
-            if (goRight) {
-                wheelPower = -wheelPower;
-            }
-
-            setDriveMotors(-wheelPower, wheelPower, -wheelPower, wheelPower);
-
-            currentHeading = gyro.getHeading();
-
-            degreesToTurn = Math.abs(target_heading - currentHeading);       // Calculate how far is remaining to turn
-
-            goRight = target_heading > currentHeading;
-
-            if (degreesToTurn > 180) {
-                goRight = !goRight;
-                degreesToTurn = 360 - degreesToTurn;
-            }
-
-            if (Math.abs(currentHeading - prevHeading) > 1) {  // if it has turned at least one degree
-                timeoutTimer.reset();
-                prevHeading = currentHeading;
-            }
-
-        }
-
-        setDriveMotors(0, 0, 0, 0);
     }
 
     /*
---
+++ Motion.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.ChampBot;
+import org.firstinspires.ftc.teamcode.Gyro;
+
+public class Motion {
+    Motion motion;
+
+    ChampBot robot = new ChampBot();   // Use robot's hardware
+
+
+    public Gyro gyro;
+
+    // gyro init
+    Gyro gyro;
+    gyro = new Gyro(robot.hwMap, "imu"); // special initialization for gyro
+    gyro.start();
+
+
+    public void turnToHeading(double target_heading, double speedModifier) {
+        boolean goRight;
+        double currentHeading;
+        double degreesToTurn;
+        double wheelPower;
+        double prevHeading = 0;
+        ElapsedTime timeoutTimer = new ElapsedTime();
+
+        currentHeading = gyro.getHeading();
+        degreesToTurn = Math.abs(target_heading - currentHeading);
+
+        goRight = target_heading > currentHeading;
+
+        if (degreesToTurn > 180) {
+            goRight = !goRight;
+            degreesToTurn = 360 - degreesToTurn;
+        }
+
+        timeoutTimer.reset();
+        prevHeading = currentHeading;
+        while (degreesToTurn > .5 /*&& opModeIsActive()*/ && timeoutTimer.seconds() < 2) {
+
+            if (speedModifier < 0) {
+                wheelPower = (Math.pow((degreesToTurn + 25) / -speedModifier, 3) + 15) / 100;
+            } else {
+                if (speedModifier != 0) {
+                    wheelPower = (Math.pow((degreesToTurn) / speedModifier, 4) + 35) / 100;
+                } else {
+                    wheelPower = (Math.pow((degreesToTurn) / 30, 4) + 15) / 100;
+                }
+            }
+
+            if (goRight) {
+                wheelPower = -wheelPower;
+            }
+
+            robot.setDriveMotors(-wheelPower, wheelPower, -wheelPower, wheelPower);
+
+            currentHeading = gyro.getHeading();
+
+            degreesToTurn = Math.abs(target_heading - currentHeading);       // Calculate how far is remaining to turn
+
+            goRight = target_heading > currentHeading;
+
+            if (degreesToTurn > 180) {
+                goRight = !goRight;
+                degreesToTurn = 360 - degreesToTurn;
+            }
+
+            if (Math.abs(currentHeading - prevHeading) > 1) {  // if it has turned at least one degree
+                timeoutTimer.reset();
+                prevHeading = currentHeading;
+            }
+
+        }
+
+        robot.setDriveMotors(0, 0, 0, 0);
+    }
+}
