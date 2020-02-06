d6a227e2907db07ba37aaddc9ec5bc8eded9a611
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2020-02-06 08:37:29 -0500

    RE and RR: breaking more things

---
+++ Motion.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Vision.VuforiaStuff;
+
+
+@Autonomous(name="Motion", group="ChampBot")
+public class Motion extends LinearOpMode {
+    ChampBot robot = new ChampBot();   // Use robot's hardware
+
+    public void DriveForward(double motorPower, double distanceInches) {
+        while (opModeIsActive()) {
+            robot.setDriveMotors(motorPower, motorPower, motorPower, motorPower); // moves away from blocks
+            if (robot.DriveFrontLeft.getCurrentPosition() >= robot.inchesToEncoderCounts(distanceInches)) {
+                robot.stopAllMotors();
+                return;
+            }
+        }
+    }
+
+    public void Turn(double motorPower, double angle, boolean right) {
+        
+    }
+}
