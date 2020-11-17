eb1b346d904c16a0448fa6f5a434514f9f8c0bbd
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2020-11-17 15:29:54 -0500

    Update WobbleBlue.java

--- WobbleBlue.java
+++ WobbleBlue.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -41,14 +12,14 @@ public class WobbleBlue extends LinearOpMode {
     ChampBot robot = new ChampBot();   // Use robot's hardware
 
     private enum States { // states for the autonomous FSM
-        DRIVE_OUT, TURN_RIGHT, TURN_LEFT, DRIVE_BACK, STOP;
+        DRIVE_OUT1, TURN_RIGHT1, TURN_RIGHT2, DRIVE_OUT2, DRIVE_OUT3, DRIVE_OUT4, DRIVE_OUT5, TURN_LEFT1, TURN_LEFT2, DRIVE_BACK2, DRIVE_BACK1, STOP;
     }
 
     public void runOpMode() {
         robot.init(hardwareMap);
         Gyro gyro;
 
-        WobbleBlue.States current_state = WobbleBlue.States.DRIVE_OUT;
+        States current_state = States.DRIVE_OUT1;
 
         // send telemetry message to signify robot waiting
         telemetry.addData("Status: ", "Snoozing");
@@ -63,48 +34,164 @@ public class WobbleBlue extends LinearOpMode {
         telemetry.update();
         sleep(100);
 
-        if (gamepad1.a) {
-            while (current_state == WobbleBlue.States.DRIVE_OUT) {
-                robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
-                if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(5.0)) {
-                    robot.stopAllMotors();
-                    current_state = States.TURN_RIGHT;
+        while (current_state == States.DRIVE_OUT1) {
+            robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(5.0)) {
+                robot.stopAllMotors();
+                current_state = States.TURN_RIGHT1;
 
-                }
-                if (!opModeIsActive()) return; // check termination in the innermost loop
             }
-                robot.resetAllEncoders();
-                telemetry.addData("Status", current_state);
-                telemetry.update();
-                sleep(100);
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
 
-            while (current_state == WobbleBlue.States.TURN_RIGHT) {
-                robot.setDriveMotors(0.4,  -0.4, 0.4, -0.4); //drive forwards
-                if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(19.21)) {
-                    robot.stopAllMotors();
-                    current_state = States.DRIVE_OUT;
+        while (current_state == States.TURN_RIGHT1) {
+            robot.setDriveMotors(0.4,  -0.4, 0.4, -0.4); //turn right
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(19.21)) {
+                robot.stopAllMotors();
+                current_state = States.DRIVE_OUT2;
 
-                }
-                if (!opModeIsActive()) return; // check termination in the innermost loop
             }
-                robot.resetAllEncoders();
-                telemetry.addData("Status", current_state);
-                telemetry.update();
-                sleep(100);
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
 
-            while (current_state == WobbleBlue.States.DRIVE_OUT) {
-                robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
-                if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(52.0)) {
-                    robot.stopAllMotors();
-                    current_state = States.DRIVE_BACK;
+        while (current_state == States.DRIVE_OUT2) {
+            robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(52.0)) {
+                robot.stopAllMotors();
+                current_state = States.DRIVE_BACK1;
 
-                }
-                if (!opModeIsActive()) return; // check termination in the innermost loop
             }
-                robot.resetAllEncoders();
-                telemetry.addData("Status", current_state);
-                telemetry.update();
-                sleep(100);
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.DRIVE_BACK1) {
+            robot.setDriveMotors(-0.4, -0.4, -0.4, -0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(45.0)) {
+                robot.stopAllMotors();
+                current_state = States.TURN_RIGHT2;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.TURN_RIGHT2) {
+            robot.setDriveMotors(0.4, -0.4, 0.4, -0.4); //turn right
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(19.21)) {
+                robot.stopAllMotors();
+                current_state = States.DRIVE_OUT3;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.DRIVE_OUT3) {
+            robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(33.0)) {
+                robot.stopAllMotors();
+                current_state = States.TURN_LEFT1;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.TURN_LEFT1) {
+            robot.setDriveMotors(-0.4, 0.4, -0.4, 0.4);//turn left
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(19.21)) {
+                robot.stopAllMotors();
+                current_state = States.DRIVE_OUT4;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.DRIVE_OUT4) {
+            robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(45.0)) {
+                robot.stopAllMotors();
+                current_state = States.TURN_LEFT2;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.TURN_LEFT2) {
+            robot.setDriveMotors(-0.4, 0.4, -0.4, 0.4); //turn left
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(19.21)) {
+                robot.stopAllMotors();
+                current_state = States.DRIVE_OUT5;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.DRIVE_OUT5) {
+            robot.setDriveMotors(0.4, 0.4, 0.4, 0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(33.0)) {
+                robot.stopAllMotors();
+                current_state = States.DRIVE_BACK2;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.DRIVE_BACK2) {
+            robot.setDriveMotors(-0.4, -0.4, -0.4, -0.4); //drive forwards
+            if (robot.DriveFrontLeft.getCurrentPosition() > robot.inchesToEncoderCounts(10.0)) {
+                robot.stopAllMotors();
+                current_state = States.STOP;
+
+            }
+            if (!opModeIsActive()) return; // check termination in the innermost loop
+        }
+        robot.resetAllEncoders();
+        telemetry.addData("Status", current_state);
+        telemetry.update();
+        sleep(100);
+
+        while (current_state == States.STOP) {
+            // stop all motors
+            robot.stopAllMotors();
+            if (!opModeIsActive()) return; // check termination in the innermost loop
         }
     }
 }
