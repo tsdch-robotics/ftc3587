9bf71ecf5339e4a9c47c0562ead06c3a2ba52241
TSDCH Robotics (LabPC-3587) <LabPC-2827@robotics.tsdch>
2020-12-16 16:56:27 -0500

    Update TankDriveTeleop.java

--- TeleOP/TankDriveTeleop.java
+++ TeleOP/TankDriveTeleop.java
@@ -59,6 +59,8 @@ public class TankDriveTeleop extends OpMode {
         robot.init(hardwareMap);
     }
 
+
+
     @Override
     public void loop() {
         // tank drive: each stick controls one side of the robot
@@ -67,6 +69,7 @@ public class TankDriveTeleop extends OpMode {
         float DriveRightPower = -gamepad1.right_stick_y;
         boolean LeftStrafe = gamepad1.dpad_left;
         boolean RightStrafe = gamepad1.dpad_right;
+
         if (RightStrafe) {
             // to right strafe, right motors towards each other, left motors away from each other
             robot.DriveFrontLeft.setPower(1);
@@ -79,25 +82,37 @@ public class TankDriveTeleop extends OpMode {
             robot.DriveFrontRight.setPower(1);
             robot.DriveBackLeft.setPower(1);
             robot.DriveBackRight.setPower(-1);
+        }else if (gamepad1.y){
+            robot.WheelMotor.setPower(1);
+        }else if (gamepad1.a) {
+            robot.WheelMotor.setPower(-1);
+        }else if (gamepad1.dpad_up) {
+            robot.ArmMotor.setPower(1);
+        }else if (gamepad1.dpad_down) {
+            robot.ArmMotor.setPower(-1);
+        }else if (gamepad1.b) {
+            robot.WheelMotor.setPower(0);
+            robot.ArmMotor.setPower(0);
         }
 
-        if (gamepad1.left_bumper) {
-            DriveLeftPower = DriveLeftPower / 2;
-            DriveRightPower = DriveRightPower /2;
-        }
 
-        robot.setDriveMotors(DriveLeftPower, DriveRightPower, DriveLeftPower, DriveRightPower);
+            if (gamepad1.left_bumper) {
+                DriveLeftPower = DriveLeftPower / 2;
+                DriveRightPower = DriveRightPower / 2;
+            }
 
-        // Arm
-        //robot.Arm.setPower(-gamepad2.left_stick_y / 2);
+            robot.setDriveMotors(DriveLeftPower, DriveRightPower, DriveLeftPower, DriveRightPower);
 
-        //Wrist
-        //boolean Up = gamepad2.right_bumper;
-        //boolean Down = (gamepad2.right_trigger > 0.1);
+            // Arm
+            //robot.Arm.setPower(-gamepad2.left_stick_y / 2);
 
-        HEAD;
-        String ArmStatus = "";
-        String ClawStatus = "";
+            //Wrist
+            //boolean Up = gamepad2.right_bumper;
+            //boolean Down = (gamepad2.right_trigger > 0.1);
+
+            //HEAD;
+            //String ArmStatus = "";
+            //String ClawStatus = "";
 
         /*if(gamepad2.right_bumper) {
             robot.Wrist.setPosition(0.0);
@@ -160,3 +175,7 @@ public class TankDriveTeleop extends OpMode {
         }
     }
 }
+
+         */
+        }
+    }
