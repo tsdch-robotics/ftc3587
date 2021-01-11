1d6d85de7d9ff9e62e09a26df12c2ebaf8af00b1
TSDCH Robotics (LabPC-5459) <LabPC-5459@robotics.tsdch>
2021-01-11 13:50:14 -0500

    Update WobbleBlue.java

--- WobbleBlue.java
+++ WobbleBlue.java
@@ -18,6 +18,7 @@ public class WobbleBlue extends LinearOpMode {
     DcMotor FrontRightMotor;
     DcMotor BackLeftMotor;
     DcMotor BackRightMotor;
+    DcMotor ArmMotor;
     int milliseconds = 0;
     double LeftPower = 0;
     double RightPower = 0;
@@ -32,43 +33,50 @@ public class WobbleBlue extends LinearOpMode {
         FrontRightMotor = hardwareMap.dcMotor.get("DriveFrontRight");
         BackLeftMotor = hardwareMap.dcMotor.get("DriveBackLeft");
         BackRightMotor = hardwareMap.dcMotor.get("DriveBackRight");
+        ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
         FrontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         FrontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         BackLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        DriveRobot(260,.5,.5); //move forward 5 in
+        DriveRobot(260,.5,.5,0); //move forward 5 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(1200,.5,-.5); //turn right
+        DriveRobot(1200,.5,-.5,0); //turn right
         sleep(1000);//wait for 1 sec
-        DriveRobot(3117,.5,.5); //move forward 60 in
+        DriveRobot(3117,.5,.5,0); //move forward 60 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(2753,-.5,-.5); //move backwards 53 in
+        DriveRobot(2753,-.5,-.5,0); //move backwards 53 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(1200,.5,-.5); // turn right
+        DriveRobot(1200,.5,-.5,0); // turn right
         sleep(1000);//wait for 1 sec
-        DriveRobot(1455,.5,.5); //move forward 28 in
+        DriveRobot(1455,.5,.5,0); //move forward 28 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(1200,-.5,.5); // turn left
+        DriveRobot(1200,-.5,.5,0); // turn left
         sleep(1000);//wait for 1 sec
-        DriveRobot(2753,.5,.5); // move forward 53 in
+        DriveRobot(2753,.5,.5,0); // move forward 53 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(1200,-.5,.5); // turn left
+        DriveRobot(1200,-.5,.5,0); // turn left
         sleep(1000);//wait for 1 sec
-        DriveRobot(1455,.5,.5); // move forward 28 in
+        DriveRobot(1455,.5,.5,0); // move forward 28 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(519,-.5,-.5); // move backwards 10 in
+        DriveRobot(519,-.5,-.5,0); // move backwards 10 in
         sleep(1000);//wait for 1 sec
 
     }
     public void runOpMode2() throws InterruptedException {
-        DriveRobot(4896, .5,.5);//move forward 94.25 in
+        DriveRobot(4896, .5,.5,0);//move forward 94.25 in
         sleep(1000);//wait for 1 sec
-        DriveRobot(4896, -.5,-.5);//move backward 94.25 in
-        DriveRobot(4896, .5,.5);//move forward 94.25 in
-        DriveRobot(4896, .5,.5);//move backward 94.25 in
+        DriveRobot(4896, -.5,-.5,0);//move backward 94.25 in
+        sleep(1000);//wait for 1 sec
+        DriveRobot(4896, .5,.5,0);//move forward 94.25 in
+        sleep(1000);//wait for 1 sec
+        DriveRobot(1182, -.5,-.5,0);//move backward 22.75 in
+    }
+
+    public void runOpMode3() throws InterruptedException {
+        //DriveRobot(6708,.5,.5);
     }
 
 
-    private void DriveRobot(int milliseconds, double LeftPower, double RightPower) {
+    private void DriveRobot(int milliseconds, double LeftPower, double RightPower, double ArmPower) {
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
@@ -85,6 +93,7 @@ public class WobbleBlue extends LinearOpMode {
         FrontRightMotor.setPower(RightPower);
         BackLeftMotor.setPower(LeftPower);
         BackRightMotor.setPower(RightPower);
+        ArmMotor.setPower(ArmPower);
 
 
         sleep(milliseconds);        // wait for x seconds.
@@ -95,5 +104,6 @@ public class WobbleBlue extends LinearOpMode {
         FrontRightMotor.setPower(0);
         BackLeftMotor.setPower(0);
         BackRightMotor.setPower(0);
+        ArmMotor.setPower(0);
     }
 }
